a920918ae9d4fee4a7278a504d24255bb793e05d
Santiago Bacaro <sbacarob@outlook.com>
2019-03-24 00:28:52 +0100

    use development library to get some style changes

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule RubiksTimer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ratatouille, "~> 0.4.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      #{:ratatouille, "~> 0.4.2"},
+      {:ratatouille, git: "https://github.com/ndreynolds/ratatouille"},
     ]
   end
 end
